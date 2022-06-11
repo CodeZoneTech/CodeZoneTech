ec51a1fe0ed810cea4b212b10c17c5fa4f5936a3
Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>
2022-06-11 11:42:10 +0530

    Added BleedCode 2.0

---
+++ bleedcode2.0/js/Staircase - Manage.js
@@ -0,0 +1,7 @@
+document.getElementById("Staircase - Manage").style.visibility = "hidden";
+
+function makabase() {
+    document.getElementById("header").style.visibility = "hidden";
+    document.getElementById("Staircase - Manage").style.visibility = "visible";
+    document.getElementById("Staircase - Manage").style.marginTop = "-290px";
+}
